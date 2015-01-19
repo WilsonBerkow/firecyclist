ee9cc132632112ea55374531d8b1bf96182edc30
Wilson Berkow <wilsonberkow@gmail.com>
2015-01-18 21:27:41 -0500

    Improve Fireball appearence, and reduce it's number of Forms, for performance.

--- elm-src/Fireball.elm
+++ elm-src/Fireball.elm
@@ -17,40 +17,18 @@ configFireball = let side_len = 12
                     , side_len = side_len
                     , padded_len = side_len * 1.5  -- This is for offsetting fireballs in sequence (i.e. padded_len is the distance from one's x-/y-pos to the next's x-/y-pos).
                     }
---tri : Float -> Collage.Shape -- Makes an equilateral triangle of side-length s, pointed upward (like /\, not \/).
---tri s = let h = s * ((sqrt 3) / 2)
---        in Collage.polygon [(-s/2, -h/2), (s/2, -h/2), (0, h/2)]
-fb_height = 4 * 4.5
-single_fb =
-  let unit = 4 -- TODO!!!! Change the rest of the program to deal with these larger fireballs (I changed the unit from 3 to 4 because it looks better).
-      sq = Collage.filled Color.darkOrange (Collage.square unit)
-      at x y = Collage.move (x * unit, -y * unit) sq
-  in Collage.moveY (4.5 * unit) <| Collage.group
-      [ Collage.move (2.5*unit,-1.5*unit) (Collage.rotate (turns 0.75) (Collage.filled Color.orange (Collage.ngon 3 (3*unit))))
-      
-      , at 0   0
-      , at 1   0
-      , at 2   0
-      , at 3   0
-      , at 4   0
-      , at 5   0
-      
-      , at 0.5 1
-      , at 1   2
-      , at 1.5 3
-      , at 2   4
-      
-      , at 2.5 5
-      , at 3   4
-      , at 3.5 3
-      , at 4   2
-      , at 4.5 1
-      ]
+fb_height = 4 * 4.5 -- TODO: CHANGE THIS AND RELATED VARIABLES TO DESCRIBE THE NEWLY-SHAPED FIREBALLS.
 
 stepFireball : FireballInputs -> Fireball -> Fireball
 stepFireball dt fb = { pos = vect_fall fb.speed fb.pos, speed = fb.speed }
+
+single_fb = Collage.group [
+   Collage.move (2.5*4,-1.9*4) (Collage.rotate (turns 0.75) (Collage.filled Color.red (Collage.ngon 3 (3*4))))
+ , Collage.move (2.5*4,0) (Collage.filled Color.orange (Collage.circle 10))
+ , Collage.move (2.5*5-3,0) (Collage.outlined (Collage.solid Color.red) (Collage.circle 10))
+ ]
+
 renderFireball f = single_fb |> move_f f.pos
 
 makeFireball : Position -> Fireball
 makeFireball pos = { pos = pos, speed = configFireball.def_speed }
-staticFb x y = { pos = {x=x,y=y}, speed = 0 }
